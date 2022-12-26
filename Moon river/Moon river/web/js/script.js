2d7ae52164746f49619ad562ec67b5ef03e5ecf2
IlyaYudenkov <119615828+IlyaYudenkov@users.noreply.github.com>
2022-12-26 18:53:16 +0300

    Add files via upload

--- Moon river/Moon river/web/js/script.js
+++ Moon river/Moon river/web/js/script.js
@@ -41,3 +41,52 @@ prevSlide = () =>{
 }
 prev.addEventListener('click', prevSlide);
 next.addEventListener('click', nextSlide);
+
+
+
+const pointOne = document.querySelector('.location__point')
+const pointOneAddress = document.querySelector('.pointOne')
+const pointTwo = document.querySelector('.location__point2')
+const pointTwoAddress = document.querySelector('.pointTwo')
+const pointThree = document.querySelector('.location__point3')
+const pointThreeAddress = document.querySelector('.pointThree')
+const pointFour = document.querySelector('.location__point4')
+const pointFourAddress = document.querySelector('.pointFour')
+const pointFive = document.querySelector('.location__point5')
+const pointFiveAddress = document.querySelector('.pointFive')
+
+
+
+pointOne.addEventListener('mouseover', () => {
+    pointOneAddress.style.display = 'block';
+})
+pointOne.addEventListener('mouseout', () => {
+    pointOneAddress.style.display = 'none';
+})
+
+pointTwo.addEventListener('mouseover', () => {
+    pointTwoAddress.style.display = 'block';
+})
+pointTwo.addEventListener('mouseout', () => {
+    pointTwoAddress.style.display = 'none';
+})
+pointThree.addEventListener('mouseover', () => {
+    pointThreeAddress.style.display = 'block';
+})
+pointThree.addEventListener('mouseout', () => {
+    pointThreeAddress.style.display = 'none';
+})
+
+pointFour.addEventListener('mouseover', () => {
+    pointFourAddress.style.display = 'block';
+})
+pointFour.addEventListener('mouseout', () => {
+    pointFourAddress.style.display = 'none';
+})
+
+pointFive.addEventListener('mouseover', () => {
+    pointFiveAddress.style.display = 'block';
+})
+pointFive.addEventListener('mouseout', () => {
+    pointFiveAddress.style.display = 'none';
+})
