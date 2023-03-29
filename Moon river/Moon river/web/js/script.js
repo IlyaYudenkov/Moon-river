a301c33bfd4b8c3ec9eb4339e980b90c0836498c
IlyaYudenkov <119615828+IlyaYudenkov@users.noreply.github.com>
2023-03-29 21:13:55 +0300

    Add files via upload

--- Moon river/Moon river/web/js/script.js
+++ Moon river/Moon river/web/js/script.js
@@ -3,16 +3,10 @@ const prev = document.querySelector('.pagination__prev')
 const next = document.querySelector('.pagination__next')
 
 const pointOne = document.querySelector('.location__point')
-const pointOneAddress = document.querySelector('.pointOne')
 const pointTwo = document.querySelector('.location__point2')
-const pointTwoAddress = document.querySelector('.pointTwo')
 const pointThree = document.querySelector('.location__point3')
-const pointThreeAddress = document.querySelector('.pointThree')
 const pointFour = document.querySelector('.location__point4')
-const pointFourAddress = document.querySelector('.pointFour')
 const pointFive = document.querySelector('.location__point5')
-const pointFiveAddress = document.querySelector('.pointFive')
-
 
 const langRu = document.querySelector('.lang__ru')
 const langEng = document.querySelector('.lang__eng')
@@ -27,22 +21,6 @@ const burgerHeader = document.querySelector('.header__burger')
 const burgerHidden = document.querySelector('.hidden__burger')
 const wrapperHidden = document.querySelector('.wrapper-hidden')
 
-
-const jewelry = document.querySelector('.jewelry')
-const jewelryMenu = document.querySelector('.jewelry__menu')
-const clock = document.querySelector('.clock')
-const clockMenu = document.querySelector('.clock__menu')
-const flavors = document.querySelector('.flavors')
-const flavorsMenu = document.querySelector('.flavors__menu')
-const brands = document.querySelector('.brands')
-const brandsMenu = document.querySelector('.brands__menu')
-const leather = document.querySelector('.leather')
-const leatherMenu = document.querySelector('.leather__menu')
-const wedding = document.querySelector('.wedding')
-const weddingMenu = document.querySelector('.wedding__menu')
-const other = document.querySelector('.other')
-const otherMenu = document.querySelector('.other__menu')
-const sections = document.querySelector('.sections')
 const footer = document.querySelector('.hidden__footer')
 const wrapper = document.querySelector('.wrapper')
 
@@ -92,35 +70,13 @@ prevSlide = () => {
 }
 
 
-displayBlock = () => {
-    pointOneAddress.style.display = 'block';
+function displayBlock() {
+    this.querySelector('p').style.display = 'block';
+
 }
-displayNone = () => {
-    pointOneAddress.style.display = 'none';
-}
-displayBlock2 = () => {
-    pointTwoAddress.style.display = 'block';
-}
-displayNone2 = () => {
-    pointTwoAddress.style.display = 'none';
-}
-displayBlock3 = () => {
-    pointThreeAddress.style.display = 'block';
-}
-displayNone3 = () => {
-    pointThreeAddress.style.display = 'none';
-}
-displayBlock4 = () => {
-    pointFourAddress.style.display = 'block';
-}
-displayNone4 = () => {
-    pointFourAddress.style.display = 'none';
-}
-displayBlock5 = () => {
-    pointFiveAddress.style.display = 'block';
-}
-displayNone5 = () => {
-    pointFiveAddress.style.display = 'none';
+function displayNone() {
+    this.querySelector('p').style.display = 'none';
+
 }
 
 wordsRu = () => {
@@ -131,19 +87,13 @@ wordsRu = () => {
 
 translateEn = () => {
     for (let i = 0; i < translateArr.length; i++) {
-        translateArr[i].innerText = translateArr[i].id;
+        translateArr[i].innerText = translateArr[i].dataset.translate;
     }
-    placeholder1.placeholder = placeholder1.id;
-    placeholder2.placeholder = placeholder2.id;
-    placeholder3.placeholder = placeholder3.id;
-    placeholder4.placeholder = placeholder4.id;
-    clockMenu.classList.toggle('_change');
-    flavorsMenu.classList.toggle('_change');
-    brandsMenu.classList.toggle('_change');
-    leatherMenu.classList.toggle('_change');
-    weddingMenu.classList.toggle('_change');
-    otherMenu.classList.toggle('_change');
-    langEng.classList.toggle('_langSelected');
+    placeholder1.placeholder = placeholder1.dataset.translate;
+    placeholder2.placeholder = placeholder2.dataset.translate;
+    placeholder3.placeholder = placeholder3.dataset.translate;
+    placeholder4.placeholder = placeholder4.dataset.translate;
+    langEng.classList.add('_langSelected');
     langRu.classList.remove('_langSelected');
 }
 
@@ -155,13 +105,7 @@ translateRu = () => {
     placeholder2.placeholder = 'Введите адрес';
     placeholder3.placeholder = 'Введите E-mail';
     placeholder4.placeholder = 'Введите E-mail';
-    clockMenu.classList.remove('_change');
-    flavorsMenu.classList.remove('_change');
-    brandsMenu.classList.remove('_change');
-    leatherMenu.classList.remove('_change');
-    weddingMenu.classList.remove('_change');
-    otherMenu.classList.remove('_change');
-    langRu.classList.toggle('_langSelected');
+    langRu.classList.add('_langSelected');
     langEng.classList.remove('_langSelected');
 }
 
@@ -174,121 +118,6 @@ closeHidden = () => {
     wrapper.style.display = 'block';
 }
 
-showJewelry = () => {
-    jewelryMenu.style.display = 'flex';
-    sections.style.display = 'block';
-    closeClock();
-    closeBrands();
-    closeFlavors();
-    closeLeather();
-    closeWedding();
-    closeOther();
-}
-closeJewelry = () => {
-    jewelryMenu.style.display = 'none';
-    footer.style.display = 'block';
-}
-
-showClock = () => {
-    clockMenu.style.display = 'flex';
-    sections.style.display = 'block';
-    closeJewelry();
-    closeBrands();
-    closeFlavors();
-    closeLeather();
-    closeWedding();
-    closeOther();
-}
-closeClock = () => {
-    clockMenu.style.display = 'none';
-    footer.style.display = 'block';
-}
-
-
-showFlavors = () => {
-    flavorsMenu.style.display = 'flex';
-    sections.style.display = 'block';
-    closeJewelry();
-    closeClock();
-    closeBrands();
-    closeLeather();
-    closeWedding();
-    closeOther();
-}
-
-closeFlavors = () => {
-    flavorsMenu.style.display = 'none';
-    footer.style.display = 'block';
-}
-
-showBrands = () => {
-    brandsMenu.style.display = 'flex';
-    sections.style.display = 'block';
-    closeJewelry();
-    closeClock();
-    closeFlavors();
-    closeLeather();
-    closeWedding();
-    closeOther();
-}
-
-closeBrands = () => {
-    brandsMenu.style.display = 'none';
-    footer.style.display = 'block';
-}
-
-showLeather = () => {
-    leatherMenu.style.display = 'flex';
-    sections.style.display = 'block';
-    closeJewelry();
-    closeClock();
-    closeFlavors();
-    closeBrands();
-    closeWedding();
-    closeOther();
-}
-
-closeLeather = () => {
-    leatherMenu.style.display = 'none';
-    footer.style.display = 'block';
-}
-
-showWedding = () => {
-    weddingMenu.style.display = 'flex';
-    sections.style.display = 'block';
-    closeJewelry();
-    closeClock();
-    closeFlavors();
-    closeBrands();
-    closeLeather();
-    closeOther();
-}
-closeWedding = () => {
-    weddingMenu.style.display = 'none';
-    footer.style.display = 'block';
-}
-
-showOther = () => {
-    otherMenu.style.display = 'flex';
-    sections.style.display = 'block';
-    closeJewelry();
-    closeClock();
-    closeFlavors();
-    closeBrands();
-    closeWedding();
-}
-closeOther = () => {
-    otherMenu.style.display = 'none';
-    footer.style.display = 'block';
-}
-
-closeFooter = () => {
-    footer.style.display = 'none';
-}
-
-closeSections = () => {
-    sections.style.display = 'none';
-}
 
 wordsRu();
 
@@ -299,17 +128,17 @@ next.addEventListener('click', nextSlide);
 pointOne.addEventListener('mouseover', displayBlock)
 pointOne.addEventListener('mouseout', displayNone)
 
-pointTwo.addEventListener('mouseover', displayBlock2)
-pointTwo.addEventListener('mouseout', displayNone2)
+pointTwo.addEventListener('mouseover', displayBlock)
+pointTwo.addEventListener('mouseout', displayNone)
 
-pointThree.addEventListener('mouseover', displayBlock3)
-pointThree.addEventListener('mouseout', displayNone3)
+pointThree.addEventListener('mouseover', displayBlock)
+pointThree.addEventListener('mouseout', displayNone)
 
-pointFour.addEventListener('mouseover', displayBlock4)
-pointFour.addEventListener('mouseout', displayNone4)
+pointFour.addEventListener('mouseover', displayBlock)
+pointFour.addEventListener('mouseout', displayNone)
 
-pointFive.addEventListener('mouseover', displayBlock5)
-pointFive.addEventListener('mouseout', displayNone5)
+pointFive.addEventListener('mouseover', displayBlock)
+pointFive.addEventListener('mouseout', displayNone)
 
 langEng.addEventListener('click', translateEn);
 langRu.addEventListener('click', translateRu)
@@ -317,42 +146,4 @@ langRu.addEventListener('click', translateRu)
 
 burgerHeader.addEventListener('click', showHidden)
 burgerHidden.addEventListener('click', closeHidden)
-
-
-jewelry.addEventListener('mouseover', showJewelry)
-jewelryMenu.addEventListener('mouseout', closeJewelry)
-jewelryMenu.addEventListener('mouseout', closeSections)
-jewelry.addEventListener('mouseover', closeFooter)
-
-clock.addEventListener('mouseover', showClock)
-clockMenu.addEventListener('mouseout', closeClock)
-clockMenu.addEventListener('mouseout', closeSections)
-clock.addEventListener('mouseover', closeFooter)
-
-flavors.addEventListener('mouseover', showFlavors)
-flavorsMenu.addEventListener('mouseout', closeFlavors)
-flavorsMenu.addEventListener('mouseout', closeSections)
-flavors.addEventListener('mouseover', closeFooter)
-
-brands.addEventListener('mouseover', showBrands)
-brandsMenu.addEventListener('mouseout', closeBrands)
-brandsMenu.addEventListener('mouseout', closeSections)
-brands.addEventListener('mouseover', closeFooter)
-
-leather.addEventListener('mouseover', showLeather)
-leatherMenu.addEventListener('mouseout', closeLeather)
-leatherMenu.addEventListener('mouseout', closeSections)
-leather.addEventListener('mouseover', closeFooter)
-
-wedding.addEventListener('mouseover', showWedding)
-weddingMenu.addEventListener('mouseout', closeWedding)
-weddingMenu.addEventListener('mouseout', closeSections)
-wedding.addEventListener('mouseover', closeFooter)
-
-other.addEventListener('mouseover', showOther)
-otherMenu.addEventListener('mouseout', closeOther)
-otherMenu.addEventListener('mouseout', closeSections)
-other.addEventListener('mouseover', closeFooter)
-
-
 
